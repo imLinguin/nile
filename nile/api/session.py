42b45417e9a085cf27bdecb7db85cb2bed5f7eed
Paweł Lidwin <lidwinpawel@gmail.com>
2024-09-27 17:52:13 +0200

    fix: make pool size static

--- nile/api/session.py
+++ nile/api/session.py
@@ -5,7 +5,7 @@ class APIHandler():
     def __init__(self, config_manager):
         self.config = config_manager
         self.session = requests.Session()
-        adapter = requests.adapters.HTTPAdapter(pool_maxsize=cpu_count())
+        adapter = requests.adapters.HTTPAdapter(pool_maxsize=12)
         self.session.mount("https://", adapter)
         self.session.headers.update({
             'User-Agent': 'AGSLauncher/1.0.0'
